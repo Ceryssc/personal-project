ea076f09b8888a785c2b4662180086a8a2f27e6b
runminty <zengzhenhuang@gmail.com>
2019-04-18 11:52:40 +1200

    starting to add appbar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './Home'
+import AppBar from '@material-ui/core/AppBar'
 
 const App = () => {
+
+  appBar ; {
+    position: 'relative'
+  }
   return (
     <div>
       <h1>personal project</h1>
