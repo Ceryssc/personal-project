982bff5086ede90d84060a4cc08bac9124ad1d3a
runminty <zengzhenhuang@gmail.com>
2019-04-18 11:36:10 +1200

    added a component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './Home'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <h1>personal project</h1>
+      <Router>
+        <div>
+          <Route path='/' component={Home} />
+
+
+        </div>
+
+      </Router>
+    </div>
   )
 }
 
