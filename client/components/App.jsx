2284034ee691835ef1b20e75ff70d9f946d64195
runminty <zengzhenhuang@gmail.com>
2019-04-18 16:14:01 +1200

    nav bar experiment

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './Home'
-import AppBar from '@material-ui/core/AppBar'
+import AppBar from './AppBar'
 
 const App = () => {
 
-  appBar ; {
-    position: 'relative'
-  }
+ 
+  
   return (
     <div>
+      <AppBar/>
       <h1>personal project</h1>
       <Router>
         <div>
